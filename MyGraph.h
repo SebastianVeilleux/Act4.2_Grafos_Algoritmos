a673fc16ba81150f071f6c12a8e2151c84415ee7
Sebastian Veilleux <sevalexvs@outlook.com>
2024-11-26 23:06:02 -0600

    Refactor MyGraph header to include additional methods and data members

--- MyGraph.h
+++ MyGraph.h
@@ -1,9 +1,26 @@
+//Sebastian Alejandro Veilleux Amaya A0163
+
 #ifndef MYGRAPH_H
 #define MYGRAPH_H
+#include <vector>
+#include <stack>
+#include <queue>
 
-#include <iostream>
+using namespace std;
 
-
-
+class MyGraph{
+    private:
+        vector<vector<int>> matriz;
+        //poner como privados los metodos adicionales
+        void DFS(int nodoActual, vector<bool>& estadoActual);
+    public:
+        MyGraph(vector<vector<int>>& matriz);
+        void loadGraph(vector<vector<int>>& matriz);
+        void DFS(int nodoI);
+        void BFS(int nodoI);
+        bool isTree();
+        void topologicalSort();
+        bool bipartiteGraph();
+};
 
 #endif
